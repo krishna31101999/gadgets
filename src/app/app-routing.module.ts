e5ed740368e7928e386e034cdc3c90808746c151
krishna31101999 <bittu31101999@gmail.com>
2020-04-20 16:20:51 +0530

    krishna

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+
+
+const routes: Routes = [
+  {path:'',component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
