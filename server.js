3787ac258e68c69b95f900fe144a1fc0f11953f1
krishna31101999 <bittu31101999@gmail.com>
2020-04-20 17:16:38 +0530

    krishna changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('hi'))
+app.use(express.static(__dirname + '/dist/gadgets'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + 'dist/gadgets/index.html')))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
